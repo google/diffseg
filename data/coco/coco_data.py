7db99111cfd6188da2a88c5df2f8e05fe907d882
Junjiao Tian <samtien9388@gmail.com>
2023-12-29 13:25:13 -0500

    added license

--- data/coco/coco_data.py
+++ data/coco/coco_data.py
@@ -1,4 +1,9 @@
-# -*- coding: utf-8 -*-
+#Copyright 2023 Google LLC
+
+#Use of this source code is governed by an MIT-style
+#license that can be found in the LICENSE file or at
+#https://opensource.org/licenses/MIT.
+
 """coco_data.py
 Data pre-processing and loading pipeline for COCO-Stuff-27.
 """
